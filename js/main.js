7ca7e7057e54e9541175aac903c1ffbc63403b02
NadavZ101 <nadavzivyon@gmail.com>
2024-02-13 17:36:12 +0200

    Fix ball size

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 'use strict'
 
-var gBallSize = 50
+// var gBallSize = 0
 
 function onBallClick() {
-    gBallSize += 50
+    const sizeJump = 50
 
     const elBall = document.querySelector('.ball')
     const currHeight = parseInt(elBall.style.height)
     const currWidth = parseInt(elBall.style.width)
-    var newHeight = currHeight + gBallSize
-    var newWidth = currWidth + gBallSize
+    var newHeight = currHeight + sizeJump
+    var newWidth = currWidth + sizeJump
+    var ballSize = newHeight
 
-
-    if (gBallSize === 400) {
-        gBallSize = 0
+    if (ballSize > 400) {
         newHeight = 100
         newWidth = 100
     }
 
     elBall.style.height = newHeight + 'px'
     elBall.style.width = newWidth + 'px'
-    elBall.innerText = gBallSize
+    elBall.innerText = newWidth
+
 }
