b8b7471dbb3df7957d99b88649fcc5387596f397
NadavZ101 <nadavzivyon@gmail.com>
2024-02-13 16:11:29 +0200

    Add increment to onBallClick func and inline style to index

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 'use strict'
 
+var gBallSize = 50
+
 function onBallClick() {
-    console.log('You click on the ball')
+    const elBall = document.querySelector('.ball')
+
+    gBallSize += 50
+
+    const currHeight = elBall.style.height
+    console.log('currHeight ', currHeight)
+
+    const currWidth = elBall.style.width
+    console.log('currWidth ', currWidth)
+
+    // console.log('gBallSize ', gBallSize)
+    elBall.style.height = (currHeight + (gBallSize + 'px'))
+    elBall.style.width = (currWidth + (gBallSize + 'px'))
+    elBall.innerText = gBallSize
+
+    console.log('elBall ', elBall)
+
+
+
 }
